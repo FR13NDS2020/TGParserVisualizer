048c8b383a4b873b3d5de3fba57e3b597e8973a9
Santis Olutniks <114351871+FR13NDS2020@users.noreply.github.com>
2023-02-27 09:29:36 +0200

    adding all files

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# TGParserVisualizer
+# TGParserVisualizer - Visualize your Telegram chat history
+🚀🐍💬 TGParserVisualizer is Python script that can help you create word clouds and tables from your Telegram chat history! 📊 With its features, you can easily analyze your chat data and gain insights into your communication patterns! 👥
+
+## How to Use ?
+
+Before you can start visualizing your Telegram chat history, you need to export your chat history from Telegram. Here's how:
+	Open Telegram and go to the chat, bot, channel, or group whose chat history you want to export.
+	Click on the three dots menu at the top right corner of the chat window.
+	Select "Export chat history" from the menu.
+	Choose the format in which you want to export your chat history (e.g. HTML) and select the date range for which you want to export the chat history.
+	Click "Export" to save the chat history to your computer.
+	Once you've exported your chat history, you need to copy all the HTML files to the "messages" folder located in "/downloads/Telegram/Telegram Desktop/Chat_Export/".
+
+
+### Environment Setup
+
+Download Python 3.10. While other versions may work, compatibility cannot be guaranteed.
+
+1. Create a new virtual environment by running the following command:
+
+	'python -m venv myenv'
+
+2. Activate the virtual environment. Depending on your operating system, use one of the following commands:
+
+	On Windows:
+
+	'myenv\Scripts\activate.bat'
+
+	On Unix or Linux:
+
+	'source myenv/bin/activate'
+
+3. Install the required modules listed in the requirements.txt file by running the following command:
+
+	'pip install -r requirements.txt'
+
+
+### Parsing and Visualizing
+Once you have your chat history exported and the HTML files copied to the "messages" folder, you can start parsing and visualizing your chat history with TGParserVisualizer.
+
+### 💻Parsing 
+The first step is to parse the messages and save them to a file. To do this, open the command prompt and navigate to the folder where you downloaded TGParserVisualizer. Then, run the following command:
+
+	'python main.py -a parse'
+
+After parsing, a file named "users.txt" will be generated. This file contains a list of all the users in your chat history. You can remove users that you don't want to include in the visualization by deleting their names from the "users.txt" file.
+
+### 💭Word Cloud
+To generate a word cloud image, run the following command:
+
+'main.py -a image --dpi 300 --len 3 --max_words 100'
+
+You can adjust the parameters to customize the word cloud image:
+	--dpi: Change the DPI of the image to get a more informative image (default is 300).
+	--len: Change the minimum length of the words in the image (default is 3).
+	--max_words: Change the maximum count of words in the image. Be cautious when increasing this value, as larger numbers will slow down the process.
+	For example, you can run the following command to generate an image with a DPI of 320, a minimum word length of 4, and a maximum word count of 200:
+
+	'main.py -a image --dpi 320 -l 4 -m 200'
+
+### 📊Table
+To generate a table, run the following command:
+
+'main.py -a table --columns 10 --unic_words --len 3'
+
+
+You can adjust the parameters to customize the table:
+	--columns: Change the number of columns in the table (default is 10).
+	--unic_words: If this flag is set, only words in "unicwords.txt" will be returned.
+	--len: Change the minimum length of the words in the table (default is 3).
+	For example, you can run the following command to generate a table with 15 columns, a minimum word length of 4, and only words in "unicwords.txt":
+
+	'main.py -a table -c 15 -l'
---
+++ main.py
@@ -0,0 +1,194 @@
+from bs4 import BeautifulSoup
+import pickle
+import nltk
+import argparse
+import os
+import re
+import tqdm
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk import FreqDist
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+# create banned.txt file if it doesn't exist
+if not os.path.exists("banned.txt"):
+    open("banned.txt", 'w').close()
+
+# create unicwords.txt file if it doesn't exist
+if not os.path.exists("unicwords.txt"):
+    open("unicwords.txt", 'w').close()
+
+# create images, messages, and tables folders if they don't exist
+os.makedirs("images", exist_ok=True)
+os.makedirs("messages", exist_ok=True)
+os.makedirs("tables", exist_ok=True)
+# Ignore UserWarnings
+warnings.filterwarnings('ignore', category=UserWarning)
+
+# Set the font family for plots
+plt.rcParams['font.family'] = 'DejaVu Sans'
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(prog='Telegram_messages_parser and visualizer',
+                                 description='Parse the messages from telegram group or chat and then visualize it')
+
+# Define command line arguments
+parser.add_argument('-u', '--unic_words', help='Shows only unicwords in unicwords.txt', action='store_true', dest='unic_words')
+parser.add_argument('-l', '--len', type=int, help='Changes the minimal length of a word', default=3, action='store', dest='len')
+parser.add_argument('--dpi', type=int, help='Changes DPI of the image', default=300, action='store', dest='dpi')
+parser.add_argument('-m', '--max_words', type=int, help='Changes the maximum count of words for the image', action='store', dest='max_words')
+parser.add_argument('-a', '--action',  help='Select the action to do', default='image', choices=['image', 'table', 'parse'], required=True)
+parser.add_argument('-c', '--columns', type=int, help='Columns count for table', dest='columns')
+
+args = parser.parse_args()
+
+
+
+
+def read_words_from_file(filename):
+    """Reads words from a file and returns them as a list."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        content = f.read()
+        words = content.split(',')
+        return [word.strip().replace(' ', '') for word in words]
+
+
+def users(file_path):
+    """Reads usernames from a file and returns them as a list."""
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return list(f.read().splitlines())
+
+
+def parse():
+    messages_by_name = {}
+
+    for file_name in os.listdir("./messages"):
+        if file_name.endswith(".html"):
+            file_path = os.path.join("./messages", file_name)
+            with open(file_path, "r", encoding="utf-8") as f:
+                soup = BeautifulSoup(f, "html.parser")
+                messages = soup.select(".message.default.clearfix")
+                for i in messages:
+                    cls = len(i["class"])
+                    if cls == 3:
+                        name_elem = i.select_one("div.from_name")
+                        text = i.find("div", {"class": "text"})
+                        name = name_elem.get_text(strip=True)
+                        if name not in messages_by_name:
+                            messages_by_name[name] = []
+                        if text is not None:
+                            messages_by_name[name].append(text.text.strip())
+                    if cls == 4:
+                        previous = i.select_one("div.from_name")
+                        text = i.find("div", {"class": "text"})
+                        name1 = name
+                        if previous is not None:
+                            name1 = previous.get_text(strip=True)
+                        if text is not None:
+                            try:
+                                messages_by_name[name1].append(text.text.strip())
+                            except:
+                                pass
+    with open("messages.pkl", "wb") as f:
+        pickle.dump(messages_by_name, f)
+    with open("users.txt", "w", encoding="utf-8") as f:
+        f.write('\n'.join(messages_by_name.keys()))
+
+
+def give_pic(jobs, file_name, dpi=300, max_words=None, min_chars=3):
+    """"creates the words cloud"""
+
+    # Tokenize the data
+    text = ' '.join(jobs)
+    tokens = word_tokenize(text)
+    banned_words = read_words_from_file("banned.txt")
+    # Remove stopwords and words less than 3 characters long
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word.lower() for word in tokens if word.lower() not in stop_words and len(word) >= min_chars and word.lower() not in banned_words]
+    # Create a frequency distribution of the tokens
+    freq_dist = nltk.FreqDist(filtered_tokens)
+    # Generate a word cloud with closer together words
+    if max_words is None:
+        wordcloud = WordCloud(width=1920, height=1080, background_color='white', relative_scaling=0).generate_from_frequencies(freq_dist)
+    else:
+        wordcloud = WordCloud(width=1920, height=1080, background_color='white', max_words=max_words, relative_scaling=0).generate_from_frequencies(freq_dist)
+    # Format the filename to remove invalid characters
+    file_name = re.sub(r'[<>:"/\\|?*\x00-\x1f]', '', file_name)
+    # Display the word cloud
+    plt.figure(figsize=(12, 12), dpi=dpi)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    file_path = os.path.join("./images", file_name)
+    plt.savefig(file_path, bbox_inches='tight')
+
+
+def visualize_most_common_words(sentences, file_name, columns=10, unicwords=None, only_mat=False, min_chars=3):
+    """"creates the table from most used words"""
+
+    # Tokenize the sentences
+    banned_words = read_words_from_file("banned.txt")
+    if only_mat:
+        tokens = [word.lower() for sentence in sentences for word in word_tokenize(sentence) if word.lower() in unicwords and word.lower() not in banned_words]
+    else:
+        tokens = [word.lower() for sentence in sentences for word in word_tokenize(sentence) if len(word.lower()) >= min_chars and word.lower() not in banned_words]
+
+    # Compute the frequency distribution of the words
+    freq_dist = FreqDist(tokens)
+
+    # Check if freq_dist is empty
+    if not freq_dist:
+        return
+
+    # Get the most common words, sorted by frequency
+    most_common_words = sorted(freq_dist.items(), key=lambda x: x[1], reverse=True)[:columns]
+
+    # Compute the average frequency of all words
+    avg_frequency = sum([count for _, count in freq_dist.items()]) / len(freq_dist)
+
+    # Create a bar plot of the most common words
+    colors = sns.color_palette("flare", len(most_common_words), as_cmap=False)
+
+    plt.figure(figsize=(12, 8))
+    ax = plt.gca()
+    for i, (word, frequency) in enumerate(most_common_words):
+        ax.barh(word, frequency, align='center', height=0.7, color=colors[i])
+        ax.text(frequency + (avg_frequency * 0.01), i, str(frequency), ha='left', va='center', fontsize=10)
+    ax.set_xlabel('Frequency')
+    ax.set_ylabel('Words')
+    ax.set_title(file_name)
+
+    plt.tight_layout()
+    file_name = re.sub(r'[<>:"/\\|?*\x00-\x1f]', '', file_name)
+    file_path = os.path.join('./tables', file_name)
+    plt.savefig(file_path, bbox_inches='tight')
+
+
+def main():
+    if args.action == "parse":
+        parse()
+    elif args.action == "table":
+        with open("messages.pkl", "rb") as f:
+            soup_str = pickle.load(f)
+
+            user_list = users("users.txt")
+            u_words = read_words_from_file("unicwords.txt") if args.unicwords else None
+
+            for i in tqdm.tqdm(user_list, desc="Generating tables"):
+                messages = soup_str[i]
+                visualize_most_common_words(messages, i, columns=args.columns, only_mat=args.unic_words, unicwords=u_words, min_chars=args.len)
+    elif args.action == "image":
+        with open("messages.pkl", "rb") as f:
+            soup_str = pickle.load(f)
+
+            user_list = users("users.txt")
+
+            for i in tqdm.tqdm(user_list, desc="Generating images"):
+                messages = soup_str[i]
+                give_pic(messages, i, max_words=args.max_words, dpi=args.dpi, min_chars=args.len)
+
+
+if __name__ == "__main__":
+    main()
